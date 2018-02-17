9baacf9b81fe8ce2626aa0eec0153e4360a05695
kojoma <masha319@hotmail.co.jp>
2018-02-18 00:08:15 +0900

    プロジェクト指定して作業時間を記録する

--- handler.py
+++ handler.py
@@ -1,24 +1,53 @@
+# -*- coding: utf-8 -*-
+
 import json
+import logging
+import os
+import time
+import boto3
 
+from datetime import datetime
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+DYNAMODB_TABLE_NAME = os.environ['dynamoDBTableName']
+dynamodb = boto3.resource('dynamodb')
+dynamodb_table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
-    return response
+def record_work_time(event, context):
+    logger.info(json.dumps(event))
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    if 'project_name' in event.keys():
+        project_name = event['project_name']
+    else:
+        raise Exception("project_name is required parameter!")
+
+    if 'start_datetime' in event.keys():
+        start_timestamp = __datetime_to_timestamp(event['start_datetime'])
+    else:
+        start_timestamp = datetime.today().timestamp()
+
+    if 'end_datetime' in event.keys():
+        end_timestamp = __datetime_to_timestamp(event['end_datetime'])
+    else:
+        end_timestamp = datetime.today().timestamp()
+
+    __save_work_time(project_name, start_timestamp, end_timestamp)
+
+def __datetime_to_timestamp(dt_str: str) -> float:
+    try:
+        dt = datetime.strptime(dt_str, '%Y-%m-%d %H:%M')
+    except:
+        raise Exception("start_datetime and end_datetime is '%Y-%m-%d %H:%M' format parameter!")
+
+    return dt.timestamp()
+
+def __save_work_time(project_name: str, start_timestamp: float, end_timestamp: float) -> None:
+    dynamodb_table.put_item(
+        Item={
+            'project_name': project_name,
+            'start_time': str(start_timestamp),
+            'end_time': str(end_timestamp),
+        }
+    )
